206b3537624f3cf51ea54127f18bff64fb1a24c7
controle-de-obras-EMN2 <arqvicentesantos@gmail.com>
2026-06-17 10:40:40 -0300

    Add new contract values to valoresContratos array

--- dados/metas.js
+++ dados/metas.js
@@ -29,23 +29,53 @@ const metas = {
   },
 
   valoresContratos: [
-    {
-      contrato: "00268/24",
-      valorContratual: 188927409.78,
-      totalPedido: 11298600.00,
-      totalUnitizado: 1091136.69
-    },
-    {
-      contrato: "00272/24",
-      valorContratual: 161552735.04,
-      totalPedido: 51158366.05,
-      totalUnitizado: 2264704.29
-    },
-    {
-      contrato: "00274/24",
-      valorContratual: 57501876.48,
-      totalPedido: 42493886.73,
-      totalUnitizado: 24251441.15
-    }
-  ]
-};
+  {
+    contrato: "00268/24",
+    valorContratual: 188927409.78,
+    totalPedido: 11298600.00,
+    totalUnitizado: 1091136.69
+  },
+  {
+    contrato: "00272/24",
+    valorContratual: 161552735.04,
+    totalPedido: 51158366.05,
+    totalUnitizado: 2264704.29
+  },
+  {
+    contrato: "00274/24",
+    valorContratual: 57501876.48,
+    totalPedido: 42493886.73,
+    totalUnitizado: 24251441.15
+  },
+  {
+    contrato: "00277/24",
+    valorContratual: 165043827.92,
+    totalPedido: 99673527.45,
+    totalUnitizado: 21529551.80
+  },
+  {
+    contrato: "00797/24",
+    valorContratual: 330252968.09,
+    totalPedido: 297918027.65,
+    totalUnitizado: 115681884.36
+  },
+  {
+    contrato: "00900/24",
+    valorContratual: 95688794.88,
+    totalPedido: 35059623.94,
+    totalUnitizado: 17376952.55
+  },
+  {
+    contrato: "01151/21",
+    valorContratual: 138333835.92,
+    totalPedido: 113844092.49,
+    totalUnitizado: 70672707.17
+  },
+  {
+    contrato: "03179/23",
+    valorContratual: 51575428.16,
+    totalPedido: 40625621.67,
+    totalUnitizado: 11428205.74
+  }
+]
+}
