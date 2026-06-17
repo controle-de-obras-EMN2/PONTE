34e6a025ead550e451464d8ece5e8ebacda1d6a3
controle-de-obras-EMN2 <arqvicentesantos@gmail.com>
2026-06-17 10:13:57 -0300

    Add metas.js with financial and production data

---
+++ dados/metas.js
@@ -0,0 +1,51 @@
+const metas = {
+  atualizacao: "03/06/2026",
+
+  economias: {
+    fatorU: {
+      previsto: 21085,
+      realizado: 998
+    },
+    contrato: {
+      previsto: 18505,
+      realizado: 4508
+    }
+  },
+
+  imobilizado: {
+    previsto: 191599373.00,
+    realizado: 25179529.76
+  },
+
+  producao: {
+    integra: {
+      previsto: 59969.24,
+      realizado: 25875.76
+    },
+    andamento: {
+      previsto: 2423.40,
+      realizado: 962.55
+    }
+  },
+
+  valoresContratos: [
+    {
+      contrato: "00268/24",
+      valorContratual: 188927409.78,
+      totalPedido: 11298600.00,
+      totalUnitizado: 1091136.69
+    },
+    {
+      contrato: "00272/24",
+      valorContratual: 161552735.04,
+      totalPedido: 51158366.05,
+      totalUnitizado: 2264704.29
+    },
+    {
+      contrato: "00274/24",
+      valorContratual: 57501876.48,
+      totalPedido: 42493886.73,
+      totalUnitizado: 24251441.15
+    }
+  ]
+};
