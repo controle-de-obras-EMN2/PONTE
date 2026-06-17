6dcf95d0246bd55fe692848e3228ee751fb433c6
controle-de-obras-EMN2 <arqvicentesantos@gmail.com>
2026-06-17 13:35:45 -0300

    Add contract extension details to metas.js

--- dados/metas.js
+++ dados/metas.js
@@ -77,5 +77,64 @@ const metas = {
     totalPedido: 40625621.67,
     totalUnitizado: 11428205.74
   }
+],
+
+  extensaoContratos: [
+  {
+    contrato: "00268/24",
+    contratual: 16139.56,
+    atual: 16207.74,
+    executada: 4464.77,
+    unitizada: 127.53
+  },
+  {
+    contrato: "00272/24",
+    contratual: 20210.13,
+    atual: 11178.51,
+    executada: 7801.36,
+    unitizada: 497.85
+  },
+  {
+    contrato: "00274/24",
+    contratual: 8633.92,
+    atual: 7839.64,
+    executada: 5243.15,
+    unitizada: 1930.13
+  },
+  {
+    contrato: "00277/24",
+    contratual: 26650.21,
+    atual: 16551.35,
+    executada: 9889.31,
+    unitizada: 2628.50
+  },
+  {
+    contrato: "00797/24",
+    contratual: 33894.77,
+    atual: 29024.15,
+    executada: 15471.98,
+    unitizada: 7931.81
+  },
+  {
+    contrato: "00900/24",
+    contratual: 19586.28,
+    atual: 21332.44,
+    executada: 7493.03,
+    unitizada: 2638.68
+  },
+  {
+    contrato: "01151/21",
+    contratual: 26936.92,
+    atual: 25228.33,
+    executada: 22263.20,
+    unitizada: 15927.07
+  },
+  {
+    contrato: "03179/23",
+    contratual: 12788.14,
+    atual: 12788.14,
+    executada: 10793.75,
+    unitizada: 3132.79
+  }
 ]
 }
