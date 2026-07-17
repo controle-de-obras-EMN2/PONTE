603e6c0155d18f18b2b70fb8b0feaee5a082b7e4
controle-de-obras-EMN2 <arqvicentesantos@gmail.com>
2026-07-17 14:29:19 -0300

    Add files via upload

---
+++ MAPA/styles/0079724_ME_DO_ALTO_BAQUIRIVU_GUAUPCT_20_2_style.js
@@ -0,0 +1,81 @@
+var size = 0;
+var placement = 'point';
+function categories_0079724_ME_DO_ALTO_BAQUIRIVU_GUAUPCT_20_2(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {case 'IMOBILIZADO':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(131,0,255,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 4.787999999999999}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'OBRA A INICIAR':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(236,0,31,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 4.787999999999999}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'OBRA CONCLUÍDA':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(120,179,133,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 4.787999999999999}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'OBRA EM ANDAMENTO':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(227,200,0,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 4.787999999999999}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'SUPRIMIDO':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 4.787999999999999}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+default:
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(236,0,31,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 4.787999999999999}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_0079724_ME_DO_ALTO_BAQUIRIVU_GUAUPCT_20_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("STATUS_C");
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'line';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+    var style = categories_0079724_ME_DO_ALTO_BAQUIRIVU_GUAUPCT_20_2(feature, value, size, resolution, labelText,
+                            labelFont, labelFill, bufferColor,
+                            bufferWidth, placement);
+
+    return style;
+};
